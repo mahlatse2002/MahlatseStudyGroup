952c1eef1e3d29cde2e93ea71c1467aec517cc44
mahla <mahla@MAHLATSE>
2025-03-03 14:11:16 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MahlatseStudyGroup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
